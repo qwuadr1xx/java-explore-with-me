ffcf3b1b52c88c005e378c0303d526c3055e08d0
qwuadr1xx <qwuadrixx@gmail.com>
2025-06-24 15:38:44 +0500

    /bugfix конечное решение с докером

--- stat/server/src/main/java/ru/practicum/explorewithme/ServerApplication.java
+++ stat/server/src/main/java/ru/practicum/explorewithme/ServerApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
-        System.setProperty("spring.datasource.url", "jdbc:postgresql://localhost:5432/postgres");
-        System.setProperty("spring.datasource.username", "postgres");
-        System.setProperty("spring.datasource.password", "1234");
-        System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
-
         SpringApplication.run(ServerApplication.class, args);
     }
 }
